ccf0ae5ed7e857476c2bee3bb0488ade4f7004d3
moori <daniel.moori@gmail.com>
2016-12-06 01:55:58 -0200

    Certificado do SINDIV

--- admin/php/certificados/cert_tpl_03.php
+++ admin/php/certificados/cert_tpl_03.php
@@ -1,42 +1,42 @@
 <?php
-$cert_data["bg_tpl"] = "cert_tpl_01.png";
+$cert_data["bg_tpl"] = "cert_SINDIV_01.png";
 $cert_data["nome"] = $cert_data["nome"] != false ? $cert_data["nome"] : "";
 $cert_data["curso"] = $cert_data["curso"] != false ? $cert_data["curso"] : "";
-$cert_data["localData"] = $cert_data["localData"] != false ? $cert_data["localData"] : "em São Paulo, no dia 24 de novembro de 2016, com duração de 07 horas.";
+$cert_data["localData"] = $cert_data["localData"] != false ? $cert_data["localData"] : "";
 $cert_data["filename"] = $cert_data["filename"] != false ? $cert_data["filename"] : "temp";
-$cert_data["output"] = $cert_data["output"] != false ? $cert_data["output"] : "F"; 
+$cert_data["output"] = $cert_data["output"] != false ? $cert_data["output"] : "F";
 
 if($cert_data["texto"] == false){
-	$cert_data["texto"][] = "A Associação Brasileira de Radiologia Veterinária certifica que";
-	$cert_data["texto"][] = "participou como ouvinte do curso";
-	$cert_data["texto"][] = "apoiado pelo Colégio Brasileiro de Radiologia Veterinária
-	e realizado na Universidade Paulista (Campus Indianópolis)";
+	$cert_data["texto"][] = "participou como ouvinte do VI Simpósio Internacional de Diagnóstico";
+	$cert_data["texto"][] = "por Imagem Veterinário - SINDIV, Realizado nos dias 24 e 25 de novembro";
+	$cert_data["texto"][] = "de 2016, na cidade de Florianópolis, Santa Catarina, Brasil, promovido pela";
+	$cert_data["texto"][] = "Associação Brasileira de Radiologia Veterinária - ABRV. Carga horária: 16 horas.";
 }
 
 
 $pdf = new FPDF();
+// $pdf->AddFont('Museo','','Museo.php');
 $pdf->AddPage('L', "A4");
 
-$pdf->Image("certificados/".$cert_data["bg_tpl"],0,0, -150);
+$pdf->Image("certificados/".$cert_data["bg_tpl"],0,0, -300);
+
+
+$pdf->SetFont('Helvetica', "B",32);
+$pdf->Cell(0,60, "", 0, 1, 'C' );
+$pdf->Cell(0,16, utf8_decode($cert_data["nome"]), 0, 1, 'C' );
 
 $pdf->SetFont('Helvetica', "",18);
-$pdf->Cell(0,75, "", 0, 1, 'C' );
-$pdf->Cell(0,8, utf8_decode($cert_data["texto"][0]), 0, 1, 'C' );
-
-$pdf->SetFont('Helvetica', "B",18);
-$pdf->Cell(0,8, utf8_decode($cert_data["nome"]), 0, 1, 'C' );
+$pdf->Cell(0,8 , "", 0, 1, 'C' );
+$pdf->MultiCell(0,8, utf8_decode($cert_data["texto"][0]) , 0,'C',false);
 
 $pdf->SetFont('Helvetica', "",18);
 $pdf->MultiCell(0,8, utf8_decode($cert_data["texto"][1]) , 0,'C',false);
-
-$pdf->SetFont('Helvetica', "B",18);
-$pdf->Cell(0,8, utf8_decode($cert_data["curso"]), 0, 1, 'C' );
 
 $pdf->SetFont('Helvetica', "",18);
 $pdf->MultiCell(0,8, utf8_decode($cert_data["texto"][2]) , 0,'C',false);
 
 $pdf->SetFont('Helvetica', "",18);
-$pdf->Cell(0,8, utf8_decode($cert_data["localData"]), 0, 1, 'C' );
+$pdf->MultiCell(0,8, utf8_decode($cert_data["texto"][3]) , 0,'C',false);
 
 $fullpath = "../../download/certificados/".$cert_data["filename"] .".pdf";
 
